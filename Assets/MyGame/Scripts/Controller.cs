fa989039f5fe111cf45746be48bbb7e34bafabd6
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2021-03-09 10:03:07 +0100

    All Screen switches

--- Assets/MyGame/Scripts/Controller.cs
+++ Assets/MyGame/Scripts/Controller.cs
@@ -22,4 +22,14 @@ public class Controller : MonoBehaviour
     {
         SceneManager.LoadScene("01ColorScene");
     }
+
+    public void ChangeScene2()
+    {
+        SceneManager.LoadScene("02NumberScene");
+    }
+
+    public void ChangeScene3()
+    {
+        SceneManager.LoadScene("00WelcomeScene");
+    }
 }
